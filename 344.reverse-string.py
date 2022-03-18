0634260750e371a53bb7f9ce59c72ca1e43c121b
Raymond Tan <ray.tangent42@gmail.com>
2022-03-17 21:28:03 -0400

    Finished 207 using topological and dfs.

--- 344.reverse-string.py
+++ 344.reverse-string.py
@@ -8,7 +8,7 @@
 class Solution:
     def reverseString(self, s: List[str]) -> None:
         """
-        Do not return anything, modify s in -place instead.
+        Do ot return anything, modify s in -place instead.
         Strategy 1: 2 pointers
         Runtime: O(n)
         space: O(1)
