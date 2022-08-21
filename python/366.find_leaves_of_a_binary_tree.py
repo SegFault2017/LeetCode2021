089024a1c0b55dbea98229ddeb71e83f80b87ef4
Raymond Tan <raymondt@purposeinvest.com>
2022-08-21 11:41:42 -0400

    Done lc 366

--- python/366.find_leaves_of_a_binary_tree.py
+++ python/366.find_leaves_of_a_binary_tree.py
@@ -19,11 +19,49 @@ class Solution:
         self.pairs[depth].append(node.val)
         return depth
 
+    def dfs(self, root, parents, in_degrees) -> None:
+        if not root:
+            return 
+        in_degrees[root] = 0
+        if root.left:
+            parents[root.left] = root
+            in_degrees[root] +=1
+            self.dfs(root.left, parents, in_degrees)
+        if root.right:
+            parents[root.right] = root
+            in_degrees[root] +=1 
+            self.dfs(root.right, parents, in_degrees)
+        return
+
     def findLeaves(self, root: Optional[TreeNode]) -> List[List[int]]:
+        """
         output =[]
         self.depthOfTree(root)
         
         for _, val in self.pairs.items():
             output.append(val)
+        return output
+        """
+        parents = defaultdict(TreeNode)
+        in_degrees = defaultdict(int)
+        q = []
+        output = []
+
+        for node, val in in_degrees.items():
+            if val == 0:
+                q.append(node)
+        while q:
+            leaves = []
+            n = len(q)
+            for _ in range(n):
+                at = q.pop(0)
+                if at != root:
+                    parent = parents[at]
+                    in_degrees[parent] -=1
+                    if in_degrees[parent] == 0:
+                        q.append(parent)
+                leaves.append(at)
+            output.append(leaves)
+        
         return output
         
